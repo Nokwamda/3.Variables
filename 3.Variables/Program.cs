b5a362dd177df11f0c7b1b5f7d2b986b114118f7
Nokwanda Khubisa <Nokwanda.Khubisa@standardbank.co.za>
2024-01-10 17:19:57 +0200

    Add project files.

---
+++ 3.Variables/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3.Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declare variable
+            string name;
+            int age;
+
+            //initialize variables
+            name = "John";
+            age = 10;
+
+            Console.WriteLine("Hi "+ name);
+            Console.WriteLine("You are "+ age + " years of age.");
+            Console.WriteLine("Next year you will be " + (age + 1));
+
+            age = 10 + 10;
+
+            Console.WriteLine("In 10 years time you will be " + age);
+
+            name = "Nokwanda";
+            Console.WriteLine("Hello " + name);
+
+            //Declare and iniyialise same time!
+            string lastName = "Khubisa";
+
+            Console.WriteLine("Your last name is " + lastName);
+
+            //freeze console
+            Console.ReadLine();
+        }
+    }
+}
